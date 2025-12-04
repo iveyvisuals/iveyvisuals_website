6bcaac5a63ffcb44d55b9fec410556b3e33e432c
Isabella Villaroman <isabellavillaroman@mac.home>
2025-12-04 16:26:29 -0500

    big nav changes

--- script.js
+++ script.js
@@ -16,4 +16,27 @@ document.addEventListener("DOMContentLoaded", () => {
       observer.observe(el);
     });
   });
+
+
+const navMobile = document.querySelector('.nav-mobile');
+const navToggle = document.querySelector('.nav-toggle');
+const navSheet  = document.querySelector('.nav-sheet');
+const navLinks  = navSheet.querySelectorAll('a');
+
+navToggle.addEventListener('click', () => {
+  const isOpen = navMobile.classList.toggle('open');
+  navToggle.setAttribute('aria-expanded', isOpen);
+});
+
+// optional: close sheet when a link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMobile.classList.remove('open');
+    navToggle.setAttribute('aria-expanded', 'false');
+  });
+});
+
+  
+
+
   
